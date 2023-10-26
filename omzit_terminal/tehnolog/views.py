f40499ac52f40c722483b7323433c77eb4833479
Чекаловец А.В. <user-357@omep.net.ru>
2023-10-26 16:27:57 +0600

    added tehnolog  uploading draws form

--- omzit_terminal/tehnolog/views.py
+++ omzit_terminal/tehnolog/views.py
@@ -1,10 +1,13 @@
 import datetime
+import json
 import os
 import asyncio
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from .services.service_handlers import handle_uploaded_file
+
+from constructor.forms import QueryAnswer
+from .services.service_handlers import handle_uploaded_file, handle_uploaded_draw_file
 from .services.tech_data_get import tech_data_get
 from .forms import GetTehDataForm, ChangeOrderModel, SendDrawBack
 from scheduler.models import WorkshopSchedule
@@ -30,7 +33,14 @@ def tehnolog_wp(request):
     print(request.user.username[:8], 'tehnolog')
     if str(request.user.username).strip() != "admin" and str(request.user.username[:8]).strip() != "tehnolog":
         raise PermissionDenied
-    if request.method == 'POST':
+
+    context = {}
+    context.update(upload_draws(
+        request=request,
+        draws_path=os.path.join('C:/', 'draws'),
+        group_id=group_id
+    ))
+    if request.method == 'POST' and context['upload_alert'] == '':
         get_teh_data_form = GetTehDataForm(request.POST, request.FILES)  # класс форм с частично заполненными данными
         if get_teh_data_form.is_valid():
             # ключ excel_file в словаре request.FILES должен быть равен имени формы созданной в классе
@@ -39,10 +49,12 @@ def tehnolog_wp(request):
             # обработка выбора не excel файла
             if '.xlsx' not in filename:
                 get_teh_data_form.add_error(None, 'Файл должен быть .xlsx!')
-                context = {'get_teh_data_form': get_teh_data_form, 'alert': alert,
-                           'change_model_query_form': change_model_query_form,
-                           'send_draw_back_form': send_draw_back_form,
-                           'filter': f}
+                context.update({
+                    'get_teh_data_form': get_teh_data_form, 'alert': alert,
+                    'change_model_query_form': change_model_query_form,
+                    'send_draw_back_form': send_draw_back_form,
+                    'filter': f
+                })
                 return render(request, r"tehnolog/tehnolog.html", context=context)
             file_save_path = os.getcwd() + r'\xlsx'
             # обработчик загрузки файла
@@ -86,10 +98,12 @@ def tehnolog_wp(request):
             print(get_teh_data_form.cleaned_data)
     else:
         get_teh_data_form = GetTehDataForm()  # чистая форма для первого запуска
-    context = {'get_teh_data_form': get_teh_data_form, 'alert': alert,
-               'change_model_query_form': change_model_query_form,
-               'send_draw_back_form': send_draw_back_form,
-               'filter': f}
+    context.update({
+        'get_teh_data_form': get_teh_data_form, 'alert': alert,
+        'change_model_query_form': change_model_query_form,
+        'send_draw_back_form': send_draw_back_form,
+        'filter': f
+    })
     return render(request, r"tehnolog/tehnolog.html", context=context)
 
 
@@ -164,3 +178,69 @@ def new_model_query(request):
         else:
             pass
     return redirect('tehnolog')  # обновление страницы при успехе
+
+
+def upload_draws(request, draws_path, group_id):
+    alert = ''
+    files = dict(request.FILES).get("draw_files")
+    if request.method == 'POST' and files is not None:
+        draw_files_upload_form = QueryAnswer(request.POST, request.FILES)
+        if draw_files_upload_form.is_valid():
+            model_order_query = draw_files_upload_form.cleaned_data['model_order_query'].model_order_query
+            file_save_path = os.path.join(draws_path, model_order_query)
+
+            # создаем файл с доступами, если не существует
+            permissions_json_path = os.path.join(file_save_path, 'permissions.json')
+            if not os.path.exists(permissions_json_path):
+                try:
+                    with open(permissions_json_path, 'w') as json_file:
+                        json.dump({}, json_file)
+                except Exception as e:
+                    print(f'При попытке создания файла доступов {permissions_json_path} вызвано исключение: {e}')
+
+            success = True
+
+            for file in files:
+                filename = str(file)
+                print('-----', filename)
+                # обработчик загрузки файла
+                uploaded_file = handle_uploaded_draw_file(
+                    username=request.user.username,
+                    f=file,
+                    filename=filename,
+                    path=file_save_path
+                )
+                if not os.path.exists(uploaded_file):
+                    alert = f'Ошибка загрузки {filename}.'
+                    success = False
+                    break
+
+            if success:
+                # обновление данных в БД
+                WorkshopSchedule.objects.filter(
+                    model_order_query=model_order_query
+                ).update(
+                    td_status='передано',  # статус СЗ
+                    td_tehnolog_done_datetime=datetime.datetime.now(),  # время загрузки
+                    tehnolog_query_td_fio=f'{request.user.first_name} {request.user.last_name}',  # ФИО констр
+                    td_remarks=''
+                )
+                alert = 'Все файлы успешно загружены.'
+
+                success_group_message = (f"Передано КД. Заказ-модель "
+                                         f"{model_order_query}. "
+                                         f"Открыт доступ в папке сервера: file://svr-003/draws/"
+                                         f"{model_order_query}/. "
+                                         f"Передал КД: {request.user.first_name} {request.user.last_name}. "
+                                         f"Загружено файлов: {len(files)}.")
+                # asyncio.run(terminal_message_to_id(to_id=group_id, text_message_to_id=success_group_message))
+                print(success_group_message, group_id)
+
+        else:
+            print('INVALID FORM!')
+            alert = 'Ошибка! Форма не валидна!'
+    else:
+        draw_files_upload_form = QueryAnswer()
+
+    context = {'draw_files_upload_form': draw_files_upload_form, 'upload_alert': alert}
+    return context
