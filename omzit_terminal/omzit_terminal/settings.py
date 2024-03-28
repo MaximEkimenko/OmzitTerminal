4e4fcba9939dc446250c6c8a5359f92d31386ba7
Чекаловец А.В. <user-357@omep.net.ru>
2024-03-28 11:58:54 +0600

    report correct

--- omzit_terminal/omzit_terminal/settings.py
+++ omzit_terminal/omzit_terminal/settings.py
@@ -82,13 +82,13 @@ WSGI_APPLICATION = 'omzit_terminal.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'terminal_test',
-        'PASSWORD': 'Epass1',
-        'USER': 'admin',
+        # 'NAME': 'terminal_test',
+        # 'PASSWORD': 'Epass1',
+        # 'USER': 'admin',
 
-        # 'NAME': 'postgres',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'Valm0nts89',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Valm0nts89',
 
         'HOST': 'localhost',
         # 'HOST': '192.168.8.163',
