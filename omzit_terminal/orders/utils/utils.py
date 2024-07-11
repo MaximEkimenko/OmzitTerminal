29ae1aaebdcbad8b65be6cd18e91f138eef21f85
Соколов Д.А. <babushkian@mail.ru>
2024-07-11 15:11:44 +0600

    создает все ППР заявки на месяц, но в таблицу выводит не больше чем на следующие три дня, чтобы не загромождать вывод.

--- omzit_terminal/orders/utils/utils.py
+++ omzit_terminal/orders/utils/utils.py
@@ -193,9 +193,12 @@ def orders_get_context(request) -> dict[str, Any]:
         "materials_request",
         "revision_cause",
     ]
-
+    today_plus_3 = make_aware(datetime.now() + timedelta(days=3))
     order_table_data = (
-        Orders.fresh_orders().order_by("priority", "breakdown_date").values(*cols_extended)
+        Orders.fresh_orders()
+        .filter(breakdown_date__lt=today_plus_3)
+        .order_by("priority", "breakdown_date")
+        .values(*cols_extended)
     )
     orders_filter = get_filterset(data=request.GET, queryset=order_table_data, fields=cols)
 
@@ -440,31 +443,60 @@ ORDER_REPORT_COLUMNS = (
 
 def create_ppr_orders():
     today = date.today()
-    # номер следующего месяца
-    next_month: int = today.month + 1 if today.month + 1 < 13 else 1
-    next_month_length = calendar.monthrange(today.year, next_month)[1]
-    # дата через месяц от сегодняшнего дня, для точного определения номера месяца и года в следующем месяце
-    next_month: date = today + timedelta(days=next_month_length)
-
-    # проверяем, что в следующем месяце нет заявок
-    first_day_of_next_month = date(next_month.year, next_month.month, 1)
     next_month_orders_count = (
-        Orders.fresh_orders().filter(breakdown_date__gte=first_day_of_next_month).count()
+        Orders.fresh_orders().filter(breakdown_date__gte=today, is_ppr=True).count()
     )
-    print("количество заявок на следующий месяц:", next_month_orders_count)
-
+    print("количество заявок в этом месяце:", next_month_orders_count)
     # создаем заявки на ППР
-    ppr_equipment = Equipment.objects.exclude(ppr_plan_day__isnull=True)
+    ppr_equipment = Equipment.objects.filter(ppr_plan_day__gte=today.day).all()
     print("количество плановых ремонтов", ppr_equipment)
     if next_month_orders_count == 0:
         for equip in ppr_equipment:
-            br_date = datetime(next_month.year, next_month.month, equip.ppr_plan_day, 8)
+            br_date = datetime(today.year, today.month, equip.ppr_plan_day, 8)
             print(equip.id, br_date)
             new_ppr_order = Orders(
                 is_ppr=True,
                 equipment=equip,
                 breakdown_description="Плановый ремонт",
-                breakdown_date=br_date,
+                breakdown_date=make_aware(br_date),
+                identified_employee="Создано автоматически",
+            )
+            new_ppr_order.save()
+
+
+def create_next_month_ppr_orders():
+    """
+    Создает заявки ППР на следующий месяц по всему оборудованию, у которого имеется дата ППР
+    """
+    today = date.today()
+    # номер следующего месяца
+    next_month_number = today.month + 1 if today.month + 1 < 13 else 1
+    next_month_length = calendar.monthrange(today.year, next_month_number)[1]
+    # дата через месяц от сегодняшнего дня, для точного определения номера месяца и года в следующем месяце
+    next_month: date = today + timedelta(days=next_month_length)
+
+    # проверяем, что в следующем месяце нет заявок ППР
+    # Если их нет, значит надо создавать
+    # Если заявок на следующий месяц >0, значит задание уже отработало
+    first_day_of_next_month = date(next_month.year, next_month.month, 1)
+    next_month_orders_count = (
+        Orders.fresh_orders()
+        .filter(breakdown_date__gte=first_day_of_next_month, is_ppr=True)
+        .count()
+    )
+    # Создаем заявки на ППР.
+    # Получаем список оборудования, у которого проставлена дата ПРР и проходимся по нему,
+    # создавая на каждое оборудование заявку.
+    ppr_equipment = Equipment.objects.exclude(ppr_plan_day__isnull=True)
+    if next_month_orders_count == 0:
+        for equip in ppr_equipment:
+            # для каждого оборудования создаем свою дату заявки на основе дня ППР
+            br_date = datetime(next_month.year, next_month.month, equip.ppr_plan_day, 8)
+            new_ppr_order = Orders(
+                is_ppr=True,
+                equipment=equip,
+                breakdown_description="Плановый ремонт",
+                breakdown_date=make_aware(br_date),
                 identified_employee="Создано автоматически",
             )
             new_ppr_order.save()
