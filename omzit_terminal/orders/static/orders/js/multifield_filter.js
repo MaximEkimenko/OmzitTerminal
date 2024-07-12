7889a094a07c9dba1820c14a86137ba3cad383ac
Соколов Д.А. <babushkian@mail.ru>
2024-07-12 17:55:02 +0600

    внедрил в таблицу заявок фильтры по множеству полей, остальные фильтры убрал

--- omzit_terminal/orders/static/orders/js/multifield_filter.js
+++ omzit_terminal/orders/static/orders/js/multifield_filter.js
@@ -1,32 +1,8 @@
 "use strict";
-
-//===========================================================================
-//    служит для обработки всплывающего окна для добавления оборудования
-//===========================================================================
-const addEquipmentButton = document.querySelector("#add_equipment_button"); // Получаем кнопки с классом
-const equipmentForm = document.querySelector("#add-equipment-form");
-
-addEquipmentButton.addEventListener("click", (e) => {
-  equipmentForm.style.display = "block";
-});
-
-equipmentForm.addEventListener("click", (e) => {
-  if (!e.target.closest(".equipment_card")) {
-    equipmentForm.style.display = "none";
-  }
-});
-
-const el = document.getElementById("filter_form");
-console.log(el);
-function onChange() {
-  document.getElementById("filter_form").submit();
-}
-
 //===========================================================================
 //    фильтрует содержимое строк таблице на основании полей ввода
 //    по нескольким строкам
 //===========================================================================
-/*
 const inputs = [];
 const headItems = document.querySelectorAll("thead th");
 
@@ -69,4 +45,3 @@ function filledInputs() {
     }
   });
 }
-*/
